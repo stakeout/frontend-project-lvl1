1f9b5a451f891be261263ce6856924e90255f365
Vadim Novash <novash@tut.by>
2019-11-16 17:53:44 +0300

    brain-progression logic was finished and tested

---
+++ src/games/brain-progression.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import gameEngine from '..';
+import getRandomNumber from '../getRandomNumber';
+
+const gameCondition = 'What number is missing in the progression?';
+const game = () => {
+  const mask = '..';
+  const progressionStartDigit = 5;
+  const progressionLength = 10;
+  const maskedIndex = getRandomNumber(0, progressionLength);
+  const progressionStep = getRandomNumber(2, 4);
+  const makeProgression = (startIndex, length, step) => {
+    const result = [];
+    for (let i = startIndex; length > 0; i += step) {
+      result.push(i);
+      length -= 1;
+    }
+    return result;
+  };
+  const progression = makeProgression(progressionStartDigit, progressionLength, progressionStep);
+  const progressionWithMaskedDigit = progression.map((item, idx) => (idx === maskedIndex
+    ? mask : item));
+  const question = `${progressionWithMaskedDigit}`;
+  const correctAnswer = progression[maskedIndex];
+
+  return [question, correctAnswer];
+};
+
+export default () => gameEngine(gameCondition, game);
