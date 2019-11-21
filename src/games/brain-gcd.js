33d8d65ff65d74d5912da6ff2e2cc254d8921fd4
Vadim Novash <novash@tut.by>
2019-11-21 11:34:47 +0300

    gcd function was moved to module level

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,22 +3,22 @@ import makeGameEngine from '..';
 import getRandomNumber from '../getRandomNumber';
 
 const gameCondition = 'Find the greatest common divisor of given numbers.';
+const getGreaterCommonDivisor = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+  while (a !== 0 && b !== 0) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
+    }
+  }
+  return a + b;
+};
 const createGame = () => {
   const firstDigit = getRandomNumber(1, 25);
   const secondDigit = getRandomNumber(1, 25);
   const question = `${firstDigit} ${secondDigit}`;
-  const getGreaterCommonDivisor = (num1, num2) => {
-    let a = num1;
-    let b = num2;
-    while (a !== 0 && b !== 0) {
-      if (a > b) {
-        a %= b;
-      } else {
-        b %= a;
-      }
-    }
-    return a + b;
-  };
   const correctAnswer = getGreaterCommonDivisor(firstDigit, secondDigit);
 
   return [question, correctAnswer];
